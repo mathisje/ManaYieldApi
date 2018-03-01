fef6539fde0f69449e26692373bfa4a442752c5b
Jeff <mathisjeffr@gmail.com>
2018-02-28 22:05:07 -0800

    allow cors, mock data to return to the calculate post call for now

--- api/controllers/manaYieldController.js
+++ api/controllers/manaYieldController.js
@@ -7,5 +7,5 @@ exports.calculate = function(req, res) {
 	console.log('do stuff');
 	console.log(req.body);
 	console.log(res.body);
-    res.json(req.body);
+    res.json([1,2,3,4,5,6,7,8,9]);
 };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-let express = require("express");
+let bodyParser = require('body-parser');
+let cors = require('cors');
+let express = require('express');
 let app = express();
 let port = process.env.PORT || 3001;
-let bodyParser = require('body-parser');
   
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
 
 let routes = require('./api/routes/manaYieldRoutes'); //importing route
 routes(app); //register the route
